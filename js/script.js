e4b6dcf7de05b0b81d03f4b21862ca0d8e420c83
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-02 16:35:18 +0000

    1

--- js/script.js
+++ js/script.js
@@ -2,18 +2,35 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+'use strict'
+
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-04-HTML/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function determines museum admission price
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function check() {
+  // input
+  const sunday = document.getElementById("sunday").checked
+  const monday = document.getElementById("monday").checked
+  const tuesday = document.getElementById("tuesday").checked
+  const wednesday = document.getElementById("wednesday").checked
+  const thursday = document.getElementById("thursday").checked
+  const friday = document.getElementById("friday").checked
+  const saturday = document.getElementById("saturday").checked
+  var age = document.getElementById("age").value
+
+  // process and output
+  if ((tuesday == true || thursday == true) && (age > 12 && age < 21)) {
+    document.getElementById("museum").innerHTML = 'You are eligible for student pricing!'
+  } else {
+    document.getElementById("museum").innerHTML = 'You must pay regular price.'
+  }
 }
